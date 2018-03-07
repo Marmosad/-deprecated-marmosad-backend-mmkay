73e14878abb74bc732ee6280a21de9a0415eba3d
wen <wenyu1999@hotmail.com>
2018-03-06 21:29:03 -0500

    hotfixed silly mistake

--- marmosad-backend/app.js
+++ marmosad-backend/app.js
@@ -5,9 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -18,30 +15,32 @@ app.set('view engine', 'pug');
 app.use(favicon(path.join(__dirname, 'public', 'goose.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use('/', express.static(path.join(__dirname + '/dist/index.html')));
-app.use('/users', users);
+app.use('/', express.static(path.join(__dirname, 'public/dist')));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- marmosad-backend/routes/index.js
+++ marmosad-backend/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    app.use(express.static(path.join(__dirname + 'dist/index.html')));
 });
 
 module.exports = router;
