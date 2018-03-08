274b934c8fd75b778b5dedfaad095b6f31e7d0e3
wen <wenyu1999@hotmail.com>
2018-03-07 19:55:38 -0500

    Purge!

--- marmosad-backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    app.use(express.static(path.join(__dirname + 'dist/index.html')));
-});
-
-module.exports = router;
