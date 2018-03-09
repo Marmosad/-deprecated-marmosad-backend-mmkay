d680a3128f34bac83db2d8f155b43710308ef944
wen <wenyu1999@hotmail.com>
2018-03-08 22:38:06 -0500

    sockets prototype

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-var express = require("express");
+var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+require('./api/socket.js')(io);
+
+
 const path = require('path');
-var socketIO = require('socket.io')(http);
 const api = require('./api/api.js');
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('/api', api);
 
-app.get('/', function (req, res){
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
-
-socketIO.on('connection', function (req, res) {
-    console.log('connection established');
 });
 
 http.listen(8080, function () {
