db84419724cd2c2762047935929bd74682ef8f77
Wen Yu Ge <wenyu1999@hotmail.com>
2018-03-10 15:43:21 -0500

    Testing socket init. Most now define an API

--- api/socket.js
+++ api/socket.js
@@ -1,15 +1,17 @@
 var UserHandler = require('./userHandler.js');
 
+
 module.exports = function (io) {
     console.log('sup');
     io.sockets.on('connection', function (socket) {
+
         socket.emit('hello!');
         console.log('sup');
         var name = socket.handshake.query.name;
         var currentUser = {
-            id: socket.id,
-            name: name
-        };
+                id: socket.id,
+                name: name
+            };
 
         socket.on('ding', function () {
             socket.emit('dong', currentUser.id + ' ' + currentUser.name);
