e14a7360a9adc2bf39f8a39659a172211dd38b7b
Jonathan Yang <jonathan8589@gmail.com>
2018-03-12 06:43:22 -0400

    Added basic chat backend interface

--- api/socket.js
+++ api/socket.js
@@ -1,19 +1,40 @@
 var UserHandler = require('./userHandler.js');
 
 module.exports = function (io) {
-    console.log('sup');
+    console.log('Socket start');
     io.sockets.on('connection', function (socket) {
-        socket.emit('hello!');
-        console.log('sup');
+        socket.emit('welcome');
+
         var name = socket.handshake.query.name;
         var currentUser = {
             id: socket.id,
             name: name
         };
+        console.log('Socket ' + currentUser.id + ' opened with name ' + currentUser.name + '.');
+        socket.broadcast.emit('userJoin', currentUser.id + ' ' + currentUser.name);
 
         socket.on('ding', function () {
             socket.emit('dong', currentUser.id + ' ' + currentUser.name);
         });
+
+        socket.on('privateMessage', function (data) {
+            var socketID = UserHandler.getSocketID(data.name);
+            if(socketID){
+                socket.broadcast.to(socketID).emit('receivedMessage', {"msg":data.msg?data.msg:"", "from":currentUser.name});
+                socket.emit('msgSentSuccessfully', true);
+                console.log(currentUser.name + ' sent ' + data.msg + ' to ' + data.name + ' successfully.');
+            }
+            else{
+                socket.emit('msgSentSuccessfully', false);
+                console.log(currentUser.name + ' failed to send ' + data.msg + ' to ' + data.name);
+            }
+        });
+
+        socket.on('publicMessage', function (data) {
+            socket.broadcast.emit('receivedMessage', {"msg":data.msg?data.msg:"", "from":currentUser.name});
+            console.log(currentUser.name + ' sent ' + data.msg + ' publicly.');
+        });
+
 
         UserHandler.joined(currentUser, socket, io);
     });
